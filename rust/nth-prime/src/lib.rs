a8d506172a00977148107df73507182a2b222647
Young King <yanckin@gmail.com>
2018-10-05 00:44:13 +0800

    add rust nth-prime solution

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,3 +1,14 @@
-pub fn nth(n: u32) -> Option<u32> {
-    unimplemented!("What is the {}th prime number?", n)
+pub fn nth(n: usize) -> Option<usize> {
+    if n == 0 {
+        return None;
+    }
+    let mut primes: Vec<usize> = vec![2];
+    let mut last = primes.last().unwrap().clone();
+    while primes.len() < n {
+        last = last + 1;
+        if !primes.iter().any(|x| last % x == 0 ) {
+            primes.push(last);
+        }
+    };
+    return Some(primes[n-1]);
 }
