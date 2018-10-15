8bdf7c3b99dd941217438a24b6fc0a02aa8576b6
Young King <yanckin@gmail.com>
2018-10-15 19:48:50 +0800

    add rust solution of difference-of-squares

---
+++ rust/difference-of-squares/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn square_of_sum(n: usize) -> usize {
+    (1..n+1).fold(0, |acc, x| acc + x).pow(2)
+}
+
+pub fn sum_of_squares(n: usize) -> usize {
+    (1..n+1).map(|x| x.pow(2)).fold(0, |acc, x| acc + x)
+}
+
+pub fn difference(n: usize) -> usize {
+    square_of_sum(n) - sum_of_squares(n)
+}
